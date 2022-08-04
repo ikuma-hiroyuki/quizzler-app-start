350a718e85dc8124f7c82c9e304afea7bc9993ce
ikuma-hiroyuki <chemicalbondo@gmail.com>
2022-08-05 04:04:02 +0900

    Initial commit

---
+++ ui.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
+        self.window = tk.Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+        self.score_label = tk.Label(text="Score: 0", bg=THEME_COLOR, fg="white")
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = tk.Canvas(width=300, height=250, bg="white", highlightbackground=THEME_COLOR)
+        self.quiz_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arail", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_img = tk.PhotoImage(file="images/true.png")
+        self.true_button = tk.Button(image=true_img, highlightthickness=0, highlightbackground=THEME_COLOR,
+                                     command=self.true_process)
+        self.true_button.grid(row=2, column=0)
+
+        false_img = tk.PhotoImage(file="images/false.png")
+        self.false_button = tk.Button(image=false_img, highlightthickness=0, highlightbackground=THEME_COLOR,
+                                      command=self.false_process)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.quiz_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.quiz_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_process(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_process(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
